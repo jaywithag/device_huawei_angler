014412e17d10f653ba962dda75a4dce53651f44d
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2016-09-08 17:11:52 -0700

    Broke-it: Add broken.mk

---
+++ broken.mk
@@ -0,0 +1,29 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit Broken common.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := broken_angler
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := angler
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Jarrod Worlitz (drgroovestarr)"
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NRD90M/3085278:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90M 3085278 release-keys"
